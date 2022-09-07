5759422a4fb67e902d001d794c26b25f6317f167
Saishivareddy <saishivareddy75@gmail.com>
2022-09-07 20:13:35 +0530

    Binary Search

--- Binary_Search/binary_search.c
+++ Binary_Search/binary_search.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 int main()
 {
-	int a[100], size, i, n;
+	int arr[100], size, n, i;
 	int first, last, middle;
-	printf("Enter the size of an array :");
+	printf("Enter the size of the array :");
 	scanf("%d", &size);
-	printf("Enter %d elements :", size);
-	for (i = 0; i < size; i++)
+	printf("Enter %d elements (in ascending order) to array :", size);
+	for(i=0;i<size; i++)
 	{
-		scanf("%d", &a[i]);
-		printf("%d\t", a[i]);
+		scanf("%d", &arr[i]);
 	}
-	printf("\nEnter an element to search :");
+	printf("Array elements are :\t");
+	for(i = 0; i < size; i++)
+	{
+		printf("%d\t", arr[i]);
+	}
+	printf("\nEnter an element to search in array : ");
 	scanf("%d", &n);
 	first = 0;
 	last = size - 1;
 	middle = (first + last) / 2;
-	while (first <= last)
+	while(first <= last)
 	{
-		if (a[middle] == n)
+		if(arr[middle] == n)
 		{
-			printf("%d is found at %d position\n", n, middle + 1);
+			printf("%d is found at %d position\n", n, middle+1);
 			break;
 		}
-		else if (a[middle] < n)
+		else if(arr[middle] < n)
 		{
 			first = middle + 1;
 		}
@@ -33,7 +37,7 @@ int main()
 		}
 		middle = (first + last) / 2;
 	}
-	if (first > last)
+	if(first > last)
 	{
 		printf("%d is not found\n", n);
 	}
