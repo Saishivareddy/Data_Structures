1580f90594f882d7a8410254936c90c03f5880ff
Saishivareddy <saishivareddy75@gmail.com>
2022-09-07 19:11:36 +0530

    Linear Search

--- Linear_Search/linear_search.c
+++ Linear_Search/linear_search.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 int main()
 {
-	int a[200], size, i, n, flag = 0;
-	printf("Enter the size of an array  :");
+	int arr[100], size, n, i;
+	int flag = 0;
+	printf("Enter the size of the array : ");
 	scanf("%d", &size);
-	printf("Enter %d integers  :", size);
+	printf("Enter the %d elements to an array  :", size);
 	for (i = 0; i < size; i++)
 	{
-		scanf("%d", &a[i]);
-		printf("%d\t", a[i]);
+		scanf("%d", &arr[i]);
 	}
-	printf("\nEnter an integer to search :");
+	printf("Array elements are :\t");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d\t", arr[i]);
+	}
+	printf("\nEnter an integer to search in array :");
 	scanf("%d", &n);
 	for (i = 0; i < size; i++)
 	{
-		if (a[i] == n)
+		if (arr[i] == n)
 		{
-			printf("%d is found at %d position\n", n, i + 1);
+			printf("%d is found at %d place in array\n", n, i + 1);
 			flag = 1;
 			break;
 		}
 	}
 	if (flag == 0)
+	{
 		printf("%d is not found\n", n);
+	}
 	return 0;
 }
